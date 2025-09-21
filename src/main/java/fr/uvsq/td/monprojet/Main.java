16099a8f9cc3158e9cbadfaf9885829e4beab4a8
Mouhcene Narimane <mouhcene.narimane@gmail.com>
2025-09-21 22:56:59 +0200

    Ajout de la méthode toString dans Fraction et test dans Main

--- src/main/java/fr/uvsq/td/monprojet/Main.java
+++ src/main/java/fr/uvsq/td/monprojet/Main.java
@@ -2,6 +2,8 @@ package fr.uvsq.td.monprojet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mon projet fonctionne !");
+        // System.out.println("Mon projet fonctionne !");
+        Fraction f = new Fraction();
+        System.out.println(f);
     }
 }
